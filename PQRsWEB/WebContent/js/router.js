308ee2f7eddf30c5c3878f5d0bb21c24c077e39f
jcelemin <martinezjua00@gmail.com>
2016-10-11 21:41:39 -0500

    test

--- PQRsWEB/WebContent/js/router.js
+++ PQRsWEB/WebContent/js/router.js
@@ -6,15 +6,16 @@ define([
   'views/home/HomeView',
   'views/projects/ProjectsView',
   'views/contributors/ContributorsView',
-  'views/footer/FooterView'
-], function($, _, Backbone, HomeView, ProjectsView, ContributorsView, FooterView) {
+  'views/footer/FooterView',
+  'views/editqprs/ApproveView',
+], function($, _, Backbone, HomeView, ProjectsView, ContributorsView, FooterView,ApproveView) {
   
   var AppRouter = Backbone.Router.extend({
     routes: {
       // Define some URL routes
       'projects': 'showProjects',
       'users': 'showContributors',
-      
+      'pqrs':'showpqrs',
       // Default
       '*actions': 'defaultAction'
     }
@@ -44,6 +45,12 @@ define([
        // We have no matching route, lets display the home page 
         var homeView = new HomeView();
         homeView.render();
+    });
+     app_router.on('route:showpqrs', function (actions) {
+     
+       // We have no matching route, lets display the home page 
+        var approveView = new ApproveView();
+        approveView.render();
     });
 
     // Unlike the above, we don't call render on this view as it will handle
---
+++ PQRsWEB/WebContent/js/views/editpqrs/ApproveView.js
@@ -0,0 +1,22 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'text!templates/editpqrs/EditApprovePQRS.html'
+], function($, _, Backbone, editapprovequestion){
+
+  var ApproveQView = Backbone.View.extend({
+    el: $("#page"),
+
+    render: function(){
+      
+      $('.menu li').removeClass('active');
+      $('.menu li a[href="#pqrs"]').parent().addClass('active');
+      this.$el.html(editapprovequestion);
+    }
+
+  });
+
+  return ApproveQView;
+  
+});
