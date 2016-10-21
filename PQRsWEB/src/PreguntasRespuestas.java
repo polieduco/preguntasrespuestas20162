403d5844f1f4952bbb2c692e886b6df2ff745020
Sao912 <sak8o12@hotmail.com>
2016-10-21 09:35:46 -0500

    Clases para realizar las consultas a la DB y servicios REST

---
+++ PQRsWEB/src/PreguntasRespuestas.java
@@ -0,0 +1,51 @@
+
+ 
+import java.util.Arrays;
+import java.util.List;
+ 
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Application;
+ 
+@ApplicationPath("rest")
+@Path("PreguntasRespuestas")
+
+public class PreguntasRespuestas extends Application {
+	
+	ConexionPostgres sql = new ConexionPostgres();
+ 
+	@GET
+	@Path("getArray")
+	@Produces("application/json")
+	public List<String> getSampleData(@QueryParam("value") String value) {
+		return Arrays.asList(new String[] { "one", "two", "three", "four", value });
+	}
+ 
+	@GET
+	@Path("getString")
+	@Produces("application/json")
+	public String getString() {
+		return "Test";
+	}
+	
+	@GET
+	@Path("listarPreguntas")
+	@Produces("application/json")
+	public String listarPreguntas() {
+		return sql.consultar();
+	}
+	
+	@GET
+	@Path("filtrarPreguntas")
+	@Produces("application/json")
+	public String filtrarClientes(@QueryParam("value") int value) {
+		return sql.filtrar(value);
+	}	
+	
+	
+}
