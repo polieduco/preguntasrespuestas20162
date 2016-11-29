08027f5f99e33d307818eac9c82f41bb5348229a
amelissagomez <angemelissagomezescobar@gmail.com>
2016-11-29 00:51:28 -0500

    Se hace conexion a base de datos y se ajusta imagen(Iniciar sesion)
    Se hace conexion a base de datos de amazon y se ajusta imagen para la
    plantilla iniciar sesion

--- PQRsWEB/src/usuario/Rol.java
+++ PQRsWEB/src/usuario/Rol.java
@@ -1,8 +1,54 @@
 package usuario;
 
-public class Rol {
-	 public static void main (String args []){
-		 System.out.println("hola");
-	 }
+import java.io.IOException;
+import java.util.Properties;
 
+import RESTservice.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.MimeBodyPart;
+
+@WebServlet("/Rol")
+public class Rol extends HttpServlet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ConexionPostgres conn = new ConexionPostgres();
+		
+		
+		System.out.println(request.getParameter("username").trim());
+		String user = request.getParameter("username").trim();
+		String pass = request.getParameter("password").trim();
+		if(user == null || "".equals(user)){
+			user = "digite algun dato valido";
+			
+		}
+		String resultado=conn.findUserPassword(user,pass);
+		System.out.println("usuario: "+user+"---"+pass);	
+		System.out.println("La consulta  es : "+resultado);	
+		String result="";
+		if(resultado.equals(" ")||resultado.equals("")||resultado==null){
+			
+			result=("Error en autenticación usuario o password incorrectos");	
+			
+			
+		}else{
+			result=("Enhorabuena conectado!");	
+				
+		}
+		
+		
+		//if(resultado.split("-"))
+		response.setContentType("text/plain");
+		response.getWriter().write(result);
+	}
 }
